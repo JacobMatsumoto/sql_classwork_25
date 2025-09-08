9aeadab3a6ef842fa570dfa42ab332c6f00b099c
Jacob <jmatsumoto@students.mchenry.edu>
2025-09-07 23:51:51 -0500

    bar project

--- homework/c_a_s.sql
+++ homework/c_a_s.sql
@@ -12,13 +12,34 @@ Create a brief video of your working SQL exercise.
 
 
 
-In this sql program I'll be making a sql table for people going to a bar and signing up, and then using ALTER SELECT CASE and SELECT DISTINCT
+In this sql program I'll be making a sql table for people going to a bar and signing up, and then using ALTER to add a new section to the table, SELECT CASE to see if someones a senior and SELECT DISTINCT
 */
 
 CREATE TABLE barPatrons (
-    id INTEGER PRIMARY KEY,
-    email TEXT UNIQUE,
-    name TEXT NOT NULL,
-    age INTEGER CHECK(age >= 21),
-    favoriteDrink TEXT DEFAULT 'Water'
+    id INTEGER PRIMARY KEY, --makes id primary keys
+    email TEXT UNIQUE, --ensures people can only use an email once
+    name TEXT NOT NULL, --doesn't accept NULL as the name must be filled out with anything
+    age INTEGER CHECK(age >= 21) --makes sure they are over 21 
 );
+
+CREATE INDEX email_index ON barPatrons(email); --makes an email index
+
+ALTER TABLE barPatrons ADD favoriteDrink TEXT DEFAULT 'Water'; --adds favoriteDrink as a collum
+
+SELECT name, age, --Queries name and age then uses age to see if they're senior citizens
+    CASE WHEN age >= 65 THEN 'Senior' ELSE 'Not Senior' END AS discountStatus; FROM barPatrons
+
+SELECT DISTINCT name FROM barPatrons --Only shows distinct/unique names in the bar patron signups.
+
+INSERT INTO barPatrons (email, name, age, favoriteDrink) VALUES ('test@gmail','John Smith', 23, 'Whiskey');
+INSERT INTO barPatrons (email, name, age, favoriteDrink) VALUES ('test2@gmail','Coleene Smith', 45, 'Wine');
+INSERT INTO barPatrons (email, name, age, favoriteDrink) VALUES ('test3@gmail','Jimbo', 67, 'Bourbon');
+INSERT INTO barPatrons (email, name, age,) VALUES ('test6@gmail','John Smith', 34,); -- repeat name and no chosen drink
+SELECT * FROM barPatrons
+
+INSERT INTO barPatrons (email, age, favoriteDrink) VALUES ('test5@gmail', 43, 'Whiskey'); --no name
+INSERT INTO barPatrons (email, name, age, favoriteDrink) VALUES ('test4@gmail','Billy Thomson', 19, 'Bud Light'); -- under 21
+INSERT INTO barPatrons (email, name, age, favoriteDrink) VALUES ('test@gmail','Bill Smith', 44, 'Screw Driver'); -- repeat email
+--populating the table.
+
+DROP TABLE barPatrons;
